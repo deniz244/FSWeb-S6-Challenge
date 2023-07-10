53bfd5dc335299ff5cd48c9b393481ddd646a691
Deniz <77787415+deniz244@users.noreply.github.com>
2023-07-10 19:45:20 +0300

    css değiştirildi, app.js de loading ve error kontrolü yapıldı

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+        setLoading(false);
+        setError(false);
+        setCharacters(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        setLoading(false);
+        setError(error);
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <h1 className="Header">React Wars</h1>
+
+      {loading && <div>Loading...</div>}
+      {error && (
+        <div>
+          Error: {error.code}
+          {error.message}{" "}
+        </div>
+      )}
+
+      {characters.length > 0 &&
+        characters.map((character) => {
+          return <div>{character.name}</div>;
+        })}
     </div>
   );
-}
+};
 
 export default App;
